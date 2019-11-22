77755c7d961d62f4e06e81fc66da1d73f19b4c94
Luis Andres <luis98_mc@hotmail.com>
2019-11-21 20:40:44 -0600

    primeros avances
    menu con css y java script

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	$("#banner").css({"height":$(window).height() + "px"});
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 200){
+			if(!flag){
+				$("#logo").css({"margin-top": "-5px", "width": "50px","height":"50px","opacity": "1"});
+
+				$("header").css({"background-color": "#981e1b"});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px", "opacity":"0.8"});
+
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+
+
+	});
+
+});
