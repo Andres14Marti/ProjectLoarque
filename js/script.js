fce4d8265550a811d4fde102713415dd56c17f68
Luis Andres <luis98_mc@hotmail.com>
2019-11-22 21:23:42 -0600

    lsider imagen
    slider menu y un poco de menu responsive o adaptable

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
 			if(!flag){
 				$("#logo").css({"margin-top": "-5px", "width": "50px","height":"50px","opacity": "1"});
 
-				$("header").css({"background-color": "#965752", "opacity": "0.8"});
+				$("header").css({"background-color": "#965752", "opacity":"0.8"});
 				flag = true;
 			}
 		}else{
 			if(flag){
-				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px", "opacity":"0.8"});
+				$("#logo").css({"margin-top": "-5px", "width": "250px","height":"250px", "opacity":"0.8"});
 
 				$("header").css({"background-color": "transparent"});
 				flag = false;
