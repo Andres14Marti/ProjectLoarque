9150029c21a38dee18b5c20cfbe309e998a04ba3
Lidia Mejia <lidiamejia29@yahoo.com>
2019-11-30 09:21:22 -0600

    DimensionesV1
    Página Principal Dimensiones y comienzo Samaritana

--- js/main.js
+++ js/main.js
@@ -31,3 +31,176 @@ window.addEventListener('click',function(e){
         }
     }
 });
+
+	/*************** Efecto de las Tarjetas en DIMENSIONES ***************/
+
+	var altoTarjeta = "25px";
+	var nuevoAlto = "135px";
+
+	$(".contenidod .tarjetasd ul .samaritana").click(function() /* Click sobre samaritana */
+	{
+		if( $(this).css( "height" ) == altoTarjeta ) /* si no esta abierta */
+		{
+			/* abre la tarjeta */
+			$(this).css({"height": nuevoAlto,"transition": "0.25s ease-in-out height"});
+
+			if(screen.width >= 1024)
+			{
+				/* pone la imagen de fondo para escritorio*/
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/samaritana.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"max-width": "400px",
+					"width": "400px",
+					"min-height": "380px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+			else
+			{
+				/* pone la imagen de fondo */
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/samaritana.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"width": "330px",
+					"min-height": "330px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+
+			/* cierra todas las demas */
+			$(".contenidod .tarjetasd ul .pascual").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .comunidades").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .misionera").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});			
+		}
+	});	
+
+	$(".contenidod .tarjetasd ul .pascual").click(function() /* click sobre pascual */
+	{
+		if( $(this).css( "height" ) == altoTarjeta ) /* si no esta abierta */
+		{
+			/* abre la tarjeta */
+			$(this).css({"height": nuevoAlto,"transition": "0.25s ease-in-out height"});
+
+			if(screen.width >= 1024)
+			{
+				/* pone la imagen de fondo para escritorio*/
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/pascual.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"max-width": "400px",
+					"width": "400px",
+					"min-height": "380px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+			else
+			{
+				/* pone la imagen de fondo */
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/pascual.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"width": "330px",
+					"min-height": "330px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+
+			/* cierra todas las demas */
+			$(".contenidod .tarjetasd ul .samaritana").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .comunidades").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .misionera").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});			
+		}
+	});
+	
+	$(".contenidod .tarjetasd ul .comunidades").click(function() /* click sobre comunidades */
+	{
+		if( $(this).css( "height" ) == altoTarjeta ) /* si no esta abierta */
+		{
+			/* abre la tarjeta */
+			$(this).css({"height": nuevoAlto,"transition": "0.25s ease-in-out height"});
+
+			if(screen.width >= 1024)
+			{
+				/* pone la imagen de fondo para escritorio*/
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/comunidades.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"max-width": "400px",
+					"width": "400px",
+					"min-height": "380px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+			else
+			{
+				/* pone la imagen de fondo */
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/comunidades.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"width": "330px",
+					"min-height": "330px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+
+			/* cierra todas las demas */
+			$(".contenidod .tarjetasd ul .samaritana").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .pascual").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .misionera").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});			
+		}
+	});
+	
+	$(".contenidod .tarjetasd ul .misionera").click(function() /* click sobre misionera */
+	{
+		if( $(this).css( "height" ) == altoTarjeta ) /* si no esta abierta */
+		{
+			/* abre la tarjeta */
+			$(this).css({"height": nuevoAlto,"transition": "0.25s ease-in-out height"});
+
+			if(screen.width >= 1024)
+			{
+				/* pone la imagen de fondo para escritorio*/
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/misionera.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"max-width": "400px",
+					"width": "400px",
+					"min-height": "380px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+			else
+			{
+				/* pone la imagen de fondo */
+				$(".contenidod .imagent").css({
+					"background-image": "url(img/misionera.jpg)",
+					"background-size": "cover",
+					"background-repeat": "no-repeat",
+					"background-position": "center",
+					"width": "330px",
+					"min-height": "330px",
+					"transition": "0.5s linear background-image"
+				});
+			}
+
+			/* cierra todas las demas */
+			$(".contenidod .tarjetasd ul .samaritana").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .pascual").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});
+			$(".contenidod .tarjetasd ul .comunidades").css({"height": "25px","transition": "0.25s ease-in-out height", "overflow": "hidden"});			
+		}
+	});	
