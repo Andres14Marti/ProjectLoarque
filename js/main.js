3477610150d0decfcbea4e07c2c85a9fe6055436
Luis Andres <luis98_mc@hotmail.com>
2019-12-04 14:15:17 -0600

    pagina inicio terminado
    terminado en un 98%

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
+let boton = document.getElementById("icono");/*varaible tipo let para el icono*/ 
+let enlaces = document.getElementById("enlaces");/*variable let para los enlaces*/
 let contador = 0;
 
+
+/*cuando hagan click en el boton hamburguesa*/ 
 boton.addEventListener("click",function(){
     if(contador == 0){
         enlaces.className = ('enlaces dos');
-        contador=1;
+		contador=1;
+		/*si el contador vale 0 que me cambie al enlace 2 que esta en css*/
     }else{
-        enlaces.classList.remove('dos');
+		/*caso contrario remueve enlace dos y pasa a uno*/
+        enlaces.classList.remove('dos');//
         enlaces.className = ('enlaces uno');
         contador = 0;
     }
-})
+});
 
+/*cuando la ventana cambie de tamaño*/ 
 window.addEventListener('resize', function(){
-    if(screen.width > 1100){
-        contador=0;
+    if(screen.width > 1100){ // si la panatalla es mayor a 1100 px
+		contador=0; // contador es 0 para que se oculte
+		// se agrega lo siguiente para cambiar la funcion y se oculte el menu
         enlaces.classList.remove('dos');
         enlaces.className = ('enlaces uno');
 
     }
-})
+});
 
-window.addEventListener('click',function(e){
+
+
+/* lo sigueinte es para que no se oculte mientras no se le de click al menu hamburguesa */
+window.addEventListener('click',function(e){ //lee el evento click dentro del menu hamburguesa
     console.log(e.target);
     if(cerrado==false){
-        let span = document.querySelector('.links-header');
+        let span = document.querySelector('.links-header'); // elige el selectro del menu dentro del header
         if(e.target == span){
             contador=0;
         }
     }
+});
+
+/*funcion para boton hacia arriba*/
+
+window.onscroll = function(){
+	if(document.documentElement.scrollTop>200){
+		document.querySelector('.go-top-container').classList.add('show')
+	}
+	else{
+		document.querySelector('.go-top-container').classList.remove('show')
+	}
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click',() =>{
+	window.scrollTo({
+		top:0,
+		behavior: 'smooth'
+	});
 });
 
 	/*************** Efecto de las Tarjetas en DIMENSIONES ***************/
